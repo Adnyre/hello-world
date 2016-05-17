b39b2f04931f3ecfe168a52c53cc57d34aaf82ea
adnyre <adnyre@gmail.com>
2016-05-18 02:19:57 +0300

    sleep for 0.5 sec

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,11 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println(s);
         Test.printTest();
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        Test.exit();
     }
 }
